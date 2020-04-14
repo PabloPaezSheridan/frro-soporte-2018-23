1bccf74ad0a1eac6466843641a6ee5dbaf50a4a4
Pablo Paez <pablopaezps97@gmail.com>
2020-04-14 12:06:28 -0300

    Ej 6 terminado, confirmar que ande bien

--- practico_03/ejercicio_06.py
+++ practico_03/ejercicio_06.py
@@ -5,14 +5,36 @@
 
 # Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
 
-from practico_03.ejercicio_01 import borrar_tabla, crear_tabla
+from ejercicio_01 import borrar_tabla, crear_tabla
 
 
 def crear_tabla_peso():
+
+    conexion = sqlite3.connect("sgdpv.db")
+    cursor = conexion.cursor()
+    try:
+        sql = ("""CREATE TABLE IF NOT EXISTS PersonaPeso (
+            IdPersona INTEGER,
+            Fecha TEXT,
+            PESO INTEGER
+            PRIMARY KEY(IdPersona,Fecha)
+            FOREIGN KEY(IdPersona) REFERENCES persona(IdPersona)
+            );""")
+        cursor.execute(sql)
+        conexion.commit()
+    finally:
+        conexion.close()
     pass
 
 
 def borrar_tabla_peso():
+   def borrar_tabla():
+    conexion = sqlite3.connect("sgdpv.db")
+    cursor = conexion.cursor()
+    sql = ("DROP TABLE IF EXISTS PersonaPeso")
+    cursor.execute(sql)
+    conexion.commit()
+    conexion.close()
     pass
 
 
