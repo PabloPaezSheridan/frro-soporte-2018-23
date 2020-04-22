e31770ad4a1aeac9440f4e356acefe0a161ffbba
Ramiro <ariasramirox@gmail.com>
2020-04-22 17:44:45 -0300

    Ejercicios 1 y 2

--- practico_04/ejercicio01.py
+++ practico_04/ejercicio01.py
@@ -1,5 +1,48 @@
-## 1 Ejercicio Hacer un formulario tkinter que es una calculadora, tiene 2 entry para ingresar los valores V1 y V2.
-## Y 4 botones de operaciones para las operaciones respectivas + , - , * , / ,
-## al cliquearlos muestre el resultado de aplicar el operador respectivo en los V1 y V2 . 
- 
+# 1 Ejercicio Hacer un formulario tkinter que es una calculadora, tiene 2 entry para ingresar los valores V1 y V2.
+# Y 4 botones de operaciones para las operaciones respectivas + , - , * , / ,
+# al cliquearlos muestre el resultado de aplicar el operador respectivo en los V1 y V2. 
 
+import tkinter
+
+def calcular(operacion):
+    if not tBVariable1.get() or not tBVariable2.get():
+        print("Complete los campos")
+    else:
+        text = tBVariable1.get() + operacion + tBVariable2.get()
+        print(eval(text))
+
+
+ventana = tkinter.Tk()
+
+
+# Estiquetas
+etiqueta1 = tkinter.Label(ventana, text="Primer operando")
+etiqueta2 = tkinter.Label(ventana, text="Segundo operando")
+
+# Estiquetas en pantalla
+etiqueta1.grid(row=0, column=0, padx=5, pady=5)
+etiqueta2.grid(row=0, column=1, padx=5, pady=5)
+
+# Entrada
+tBVariable1 = tkinter.Entry(ventana)
+tBVariable2 = tkinter.Entry(ventana)
+
+# Entrada en pantalla
+tBVariable1.grid(row=1, column=0, padx=5, pady=5)
+tBVariable2.grid(row=1, column=1, padx=5, pady=5)
+
+
+# Botones
+btnSuma = tkinter.Button(ventana, text="+", width=2, heigh=1, command=lambda: calcular("+"))
+btnResta = tkinter.Button(ventana, text="-", width=2, heigh=1, command=lambda: calcular("-"))
+btnPor = tkinter.Button(ventana, text="x", width=2, heigh=1, command=lambda: calcular("*"))
+btnDiv = tkinter.Button(ventana, text="%", width=2, heigh=1, command=lambda: calcular("/"))
+
+# Botones en pantalla
+btnSuma.grid(row=1, column=2, padx=5, pady=5)
+btnResta.grid(row=1, column=3, padx=5, pady=5)
+btnPor.grid(row=1, column=4, padx=5, pady=5)
+btnDiv.grid(row=1, column=5, padx=5, pady=5)
+
+
+ventana.mainloop()
--- practico_04/ejercicio03.py
+++ practico_04/ejercicio03.py
@@ -1,2 +1,2 @@
-## 3 Ejercicio Crear un Formulario que usando el control Treeview muestre la una lista con los nombre de
-## Ciudades Argentinas y su código postal ( por lo menos 5 ciudades ) . 
+# 3 Ejercicio Crear un Formulario que usando el control Treeview muestre la una lista con los nombre de
+# Ciudades Argentinas y su código postal ( por lo menos 5 ciudades ). 
